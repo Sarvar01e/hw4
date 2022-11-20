503c0257b81ad7e6172a59b401d4d31abacdd508
user <user@WIN-AKM2LNADFM7>
2022-11-21 00:28:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hw4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+
+            int number;
+
+            number = int.Parse(Console.ReadLine());
+
+            if( number%2 == 0)
+            {
+                Console.WriteLine("Your number is honest");
+            }
+            else
+            {
+                Console.WriteLine("Your number is odd");
+            }
+   
+
+
+        }
+    }
+}
